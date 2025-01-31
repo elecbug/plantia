c5268fb3132f5ded9a2e8fe2b95912f7fcbc5f16
elecbug <deveb1479@gmail.com>
2025-02-01 01:01:14 +0900

    fix: update ore mining bug

--- src/main/java/com/elecbug/plantia/blocks/ManaOreBlock.java
+++ src/main/java/com/elecbug/plantia/blocks/ManaOreBlock.java
@@ -1,11 +1,13 @@
 package com.elecbug.plantia.blocks;
 
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 
 public class ManaOreBlock extends Block {
     public ManaOreBlock() {
-        super(BlockBehaviour.Properties.copy(Blocks.IRON_ORE));
+        super(BlockBehaviour.Properties.of(Material.STONE)
+            .strength(3.0f, 3.0f)
+            .requiresCorrectToolForDrops());
     }
 }
